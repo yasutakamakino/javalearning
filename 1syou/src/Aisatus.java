6de75bf46ca1ca90e9ad8d509dae0482337fbe01
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-10 14:44:26 +0900

    第1章を行いました。

---
+++ 1syou/src/Aisatus.java
@@ -0,0 +1,8 @@
+/**
+ * 問題1-4 表示するプログラム ②
+ */
+public class Aisatus {
+    public static void main(String[] args){
+        System.out.println("おはよう。\nこんにちは。\nこんばんは。");
+    }
+}
---
+++ 1syou/src/kantan.java
@@ -0,0 +1,10 @@
+/**
+ * 問題1-3 表示するプログラム①
+ */
+public class kantan {
+    public static void main (String[] args){
+        for(int i= 0;i<3 ;++i){
+            System.out.println("意外と\"Java\"って簡単だ。");
+        }
+    }
+}
