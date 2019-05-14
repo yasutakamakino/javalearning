14bdd924be90cd5db43868dcde9899a0763dde97
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-14 09:09:43 +0900

    3章をしました。

---
+++ 3syou/src/hikari.java
@@ -0,0 +1,11 @@
+/**
+ * 問題3-2 光を
+ */
+public class hikari {
+    public static void main(String[] args) {
+        long time = 24 * 60 * 60;
+        long kyori = 300000 * time;
+        System.out.println("光が1日に進む距離は" + kyori + "kmです");
+    }
+}
+
