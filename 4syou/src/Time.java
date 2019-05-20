7f6e683f48ccf32348298545f3d40b41e4043ac1
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-20 10:13:18 +0900

    第四章_修正１

--- 4syou/src/Time.java
+++ 4syou/src/Time.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -12,20 +13,21 @@ public class Time {
             System.out.println("現在の時刻を入力してください");
             String buf = br.readLine();           //時刻を入力
             int hour = Integer.parseInt(buf);     // int型に変換
-            System.out.println(hour);
             if (0 <= hour && hour <= 11) {        //0時から11時の間の場合
                 System.out.println("おはようございます");
             } else if (hour == 12) {              //12時の場合
                 System.out.println("お昼です");
-            } else if (hour <= 18) {              //13時から18時の間の場合
+            } else if (13 <= hour && hour <= 18) {              //13時から18時の間の場合
                 System.out.println("こんにちは");
-            } else if (hour <= 23) {              //19時から23時の間の場合
+            } else if (19 <= hour && hour <= 23) {              //19時から23時の間の場合
                 System.out.println("こんばんは");
             } else {                              //それ以外の場合
                 System.out.println("時刻の範囲を超えています");
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println(e);
+        } catch (NumberFormatException e) {
+            System.out.println("数字の形式が正しくありません");
         }
     }
 }
