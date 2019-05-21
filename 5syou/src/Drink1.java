f55ba3803806e37c271d0871ef8580ee37c7fddc
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-21 15:48:31 +0900

    第5章_修正２

--- 5syou/src/Drink1.java
+++ 5syou/src/Drink1.java
@@ -6,33 +6,33 @@ import java.io.InputStreamReader;
  * 問題5-4　list 5-1を修正したプログラム
  */
 public class Drink1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try{
+        try {
             System.out.println("飲み物は何が好きですか？");
             System.out.println("1 オレンジジュース");
             System.out.println("2 コーヒー");
             System.out.println("3 ミルク");
             System.out.println("1,2,3のどれかを選んでください。");
             String line = reader.readLine();  //数字を入力
-            int  n =Integer.parseInt(line);     //int型に変換
-            switch(n) {
+            int n = Integer.parseInt(line);     //int型に変換
+            switch (n) {
                 case 1: //入力が１の場合
-                System.out.println("オレンジジュースです。");
-                break;
+                    System.out.println("オレンジジュースです。");
+                    break;
                 case 2: //入力が２の場合
-                System.out.println("コーヒーです。");
-                break;
+                    System.out.println("コーヒーです。");
+                    break;
                 case 3: //入力３の場合
-                System.out.println("ミルクです。");
-                break;
+                    System.out.println("ミルクです。");
+                    break;
                 default: //それ以外の場合
                     System.out.println("どちらでもありません。");
                     break;
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("数字の形式が正しくありません。");
         }
     }
