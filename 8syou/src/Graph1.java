4db0848604c0e0a779f7e0c8c814ee0bfb536449
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-28 18:02:10 +0900

    スペースの位置の修正

--- 8syou/src/Graph1.java
+++ 8syou/src/Graph1.java
@@ -1,5 +1,5 @@
 /**
- *問題8-2 以下のような放物線を描くプログラム<br>
+ * 問題8-2 以下のような放物線を描くプログラム<br>
  * <pre>
  * ****************************************************************
  * *************************************************
@@ -21,18 +21,20 @@
  * </pre>
  */
 public class Graph1 {
-    public static void main(String[] args){
-        for(int i = -8 ;i < 9 ; i++){
-            printGraph(i*i);
+    public static void main(String[] args) {
+        for (int i = -8; i < 9; i++) {
+            printGraph(i * i);
         }
     }
+
     /**
      * printGraphメソッド<br>
      * 引数の数だけ"*"を出力する
+     *
      * @param x 1行あたりの"*"を表示する数
      */
-    public static void printGraph(int x){
-        for(int i =0 ;i < x ;i++){
+    public static void printGraph(int x) {
+        for (int i = 0; i < x; i++) {
             System.out.print("*");
         }
         System.out.println("");
