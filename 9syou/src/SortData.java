c4d94a546e373fd16279ecf069c6225380db33e9
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-29 17:56:24 +0900

    問9-8

---
+++ 9syou/src/SortData.java
@@ -0,0 +1,31 @@
+/**
+ * 問題9-8　配列に入っている値を昇順に並べるプログラム
+ */
+public class SortData {
+    public static void main(String[] args) {
+        int[] data = {31, 41, 59, 26, 53, 58, 97, 93, 23, 84};
+        //並べ帰る前の値の列を表示
+        System.out.println("並べ替える前");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println(" ");
+        //昇順に並べ替える処理
+        for (int i = 0; i < data.length; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+
+                if (data[i] > data[j]) {
+                    int x;
+                    x = data[j];
+                    data[j] = data[i];
+                    data[i] = x;
+                }
+            }
+        }
+        //昇順に並べ替えた後の値の列の表示
+        System.out.println("並べ替える後");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+    }
+}
