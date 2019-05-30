aa43f6d0482cb9764b95d348f58ae1e2157b0461
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-30 09:59:28 +0900

    必要な箇所にスペースを追加

--- 9syou/src/PrintArray.java
+++ 9syou/src/PrintArray.java
@@ -13,9 +13,9 @@ public class PrintArray {
     public static void main(String[] args) {
         int[][] arr = {
                 {3, 1, 4, 1},
-                {5, 9, 2, },
-                {6, 5, },
-                {3, },
+                {5, 9, 2,},
+                {6, 5,},
+                {3,},
         };
         printArray(arr); //メソッドを呼ぶ
     }
@@ -29,9 +29,9 @@ public class PrintArray {
     public static void printArray(int[][] arr) {
         System.out.println("{");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print("  {");
+            System.out.print("  { ");
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + ",");
+                System.out.print(arr[i][j] + ", ");
             }
             System.out.println("},");
 
