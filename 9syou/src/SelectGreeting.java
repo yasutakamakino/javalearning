b330bf91a01a10fadc27b08027dd8552542af38a
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-30 10:00:39 +0900

    変数の文字の中身と演算子の前後の修正

--- 9syou/src/SelectGreeting.java
+++ 9syou/src/SelectGreeting.java
@@ -2,23 +2,23 @@
  * 問題9-6 コマンドラインの引数を用いて挨拶を表示するプログラム
  */
 public class SelectGreeting {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String[] message;
         message = new String[3];
-        message[0]="おはよう!";
-        message[1]="こんにちは！";
-        message[2]="こんばんわ！";
+        message[0] = "おはよう!";
+        message[1] = "こんにちは！";
+        message[2] = "こんばんは！";
 
-            if (args.length != 1) {
-                System.out.println("使い方：java SelectGreeting 番号");
-                System.exit(0);
-            }
-            int num = Integer.parseInt(args[0]);
-            if (0 <= num && num < message.length) { //0～2の数字の場合
-                System.out.println(message[num]);
-            } else {                                //それ以外の場合
-                System.out.println("番号は0～" + (message.length-1)+"の範囲で指定してください");
+        if (args.length != 1) {
+            System.out.println("使い方：java SelectGreeting 番号");
+            System.exit(0);
+        }
+        int num = Integer.parseInt(args[0]);
+        if (0 <= num && num < message.length) { //0～2の数字の場合
+            System.out.println(message[num]);
+        } else {                                //それ以外の場合
+            System.out.println("番号は0～" + (message.length - 1) + "の範囲で指定してください");
 
-            }
+        }
     }
 }
