fdb3da42f87af91c18ec2105acd9e346e3cfd2f0
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-05-30 09:57:17 +0900

    演算子の前後の修正

--- 9syou/src/CalcData.java
+++ 9syou/src/CalcData.java
@@ -33,7 +33,7 @@ public class CalcData {
         data[4] = 5.3;
         data[5] = 9.7;
         double sum = 0.0;
-        for (int i = 0; i <data.length; i++) { //条件式// を<=⇒<に変更
+        for (int i = 0; i < data.length; i++) { //条件式// を<=⇒<に変更
             sum += data[i];
         }
         System.out.println("sum=" + sum);
