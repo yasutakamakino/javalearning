c3f31aca3671aa13eb3e305bcf2f4417b52aa78b
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-03 13:51:38 +0900

    問11-8

---
+++ 11syou/src/Rectangle2.java
@@ -0,0 +1,53 @@
+/**
+ * 問題11-8　値がマイナスの時の対処が行われているプログラム
+ */
+public class Rectangle2 {
+    int width;  //幅
+    int height; //高さ
+
+    //引数なしのコンストラクタ
+    Rectangle2() {
+        setSize(10, 20);
+    }
+
+    //引数ありのコンストラクタ
+    Rectangle2(int w, int h) {
+        setSize(w, h);
+    }
+
+    /**
+     * setSizeメソッド<br>
+     * 幅と高さを格納するメソッド
+     *
+     * @param w 　幅
+     * @param h 高さ
+     */
+    void setSize(int w, int h) {
+        if (0 > w) w = 0;
+        if (0 > h) h = 0;
+        width = w;
+        height = h;
+    }
+
+    /**
+     * getAreaメソッド<br>
+     * 長方形の面積を図るメソッド
+     *
+     * @retuen 長方形の面積
+     */
+    int getArea() {
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        Rectangle2 r1 = new Rectangle2();
+        System.out.println("r1.width=" + r1.width);
+        System.out.println("r1.height=" + r1.height);
+        System.out.println("r1.getArea=" + r1.getArea());
+        Rectangle2 r2 = new Rectangle2(-125, 45);
+        System.out.println("r2.width=" + r2.width);
+        System.out.println("r2.height=" + r2.height);
+        System.out.println("r2.getArea() =" + r2.getArea());
+    }
+
+}
