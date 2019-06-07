5d3026e3568e289b327d4643624b2f1995b28cb8
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-07 09:21:15 +0900

    問12-2

---
+++ 12syou/src/Myapplet.java
@@ -0,0 +1,14 @@
+/**
+ * 問12-2　apleat のサブクラス
+ *<pre>{@code
+ * import java.applet.*;
+ * public class Myapplet {
+ *
+ *  }
+ * }</pre>
+ */
+
+import java.applet.*;
+public class Myapplet extends Applet{
+
+}
