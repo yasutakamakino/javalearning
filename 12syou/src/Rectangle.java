31d2bbb2c687eea7e1b4475fa7f6b940d0371353
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-07 16:06:26 +0900

    = の前後にスペースを追加

--- 12syou/src/Rectangle.java
+++ 12syou/src/Rectangle.java
@@ -4,18 +4,22 @@
 public class Rectangle {
     int width;
     int height;
-    Rectangle(){
-        setSize(0,0);
+
+    Rectangle() {
+        setSize(0, 0);
     }
-    Rectangle(int width,int height){
-        setSize(width,height);
+
+    Rectangle(int width, int height) {
+        setSize(width, height);
     }
-    void setSize(int width,int height){
+
+    void setSize(int width, int height) {
         this.width = width;
-        this.height=height;
+        this.height = height;
     }
+
     @Override
-    public String toString(){
-        return "["+width+", "+height+"]";
+    public String toString() {
+        return "[" + width + ", " + height + "]";
     }
 }
