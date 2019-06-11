318e7cd7c04ef21ab30182869bb56dd42891685b
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-11 17:08:23 +0900

    問14-3

---
+++ 14syou/src/IconInfo.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+/**
+ * 問14-3
+ * 次のメソッドとフィールドを持つ、IconType,戻り値の型はImage型で、名前がgetIconであるメソッド<br>
+ * 型はintで名前はICON_16x16,値は１であるフィールド<br>
+ * 型はintで名前はICON_32x32,値は2であるフィールド
+ */
+public class IconInfo {
+    Image getIcon(int iconType);
+
+    int Icon_16x16 = 1;
+    int Icon_32x32 = 2;
+
+}
