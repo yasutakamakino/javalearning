a25913369bcea2884e4f9bf1ba3edfe6b6c9487f
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-27 13:53:09 +0900

    コメントに修正箇所

--- 16syou/src/OneBank.java
+++ 16syou/src/OneBank.java
@@ -3,16 +3,22 @@
  */
 
 public class OneBank {
+    //貯金残高
     private static int value = 0;
 
-    static synchronized void addMoney(int money) {
+    //預け入れ・引き出し
+    public static synchronized void addMoney(int money) { //「修正」publicを先頭に追加
+        //現在の残高を保存
         int currentValue = value;
-        System.out.println(Thread.currentThread());
+        //状況を表示
+        System.out.println(Thread.currentThread() + "がaddMoneyに入りました。");//「修正」出力内容の表示とコメント
+        //矛盾がないかどうかチェック
         value += money;
         if (currentValue + money != value) {
-            System.out.println(Thread.currentThread());
+            System.out.println(Thread.currentThread() + "で矛盾が発生しました！");//「修正」出力内容の表示とコメント
             System.exit(-1);
         }
-        System.out.println(Thread.currentThread());
+        //状況を表示
+        System.out.println(Thread.currentThread() + "がaddMoneyから出ました。"); //「修正」出力内容の表示とコメント
     }
 }
