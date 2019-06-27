57cf199cd0668f898c82ec636f6073038bf23429
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-27 13:52:56 +0900

    コメントに修正箇所記入

--- 16syou/src/Print_1.java
+++ 16syou/src/Print_1.java
@@ -1,15 +1,12 @@
 /**
  * 問題16-7<br>
- * 問題16-6のクラスを、Runnaableインターフェスを実装して作ってください。
+ * 問題16-6のクラスを、Runnableインターフェスを実装して作ってください。
  */
+//「修正」　Runnaable⇒Runnableに変更
 public class Print_1 {
     public static void main(String[] ars) {
-        Equal_1 a = new Equal_1();
-        Thread th = new Thread(a);
-        Astar_1 b = new Astar_1();
-        Thread sh = new Thread(b);
-        th.start();
-        sh.start();
+        new Thread(new Equal_1()).start(); //「修正」 メソッドの呼び出しの書き方を修正した。
+        new Thread(new Astar_1()).start();
     }
 }
 
@@ -18,13 +15,13 @@ class Astar_1 implements Runnable {
 
     @Override
     public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { //「修正」for文をtry文の前に出した
+            try {
                 Thread.sleep(3000);
-                System.out.println("***");
+            } catch (InterruptedException e) {
+                System.out.println("Astar_1で例外発生"); //「修正」"の方"を削除
             }
-        } catch (InterruptedException e) {
-            System.out.println("Astar_1の方で例外発生");
+            System.out.println("***"); //「修正」try文の中から後に位置変更
         }
     }
 }
@@ -34,13 +31,13 @@ class Equal_1 implements Runnable {
 
     @Override
     public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { //「修正」for文をtry文の前に出した
+            try {
                 Thread.sleep(5000);
-                System.out.println("=====");
+            } catch (InterruptedException e) {
+                System.out.println("Equal_1で例外発生"); //「修正」"の方"を削除
             }
-        } catch (InterruptedException e) {
-            System.out.println("Equal_1の方で例外発生");
+            System.out.println("=====");  //「修正」try文の中から後に位置変更
         }
     }
 }
