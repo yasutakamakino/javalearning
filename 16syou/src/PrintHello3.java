3a6a16e2e4fbc91a9b6d39b69dc7c14ea1634189
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-06-27 13:49:31 +0900

    コメントの誤字の修正

--- 16syou/src/PrintHello3.java
+++ 16syou/src/PrintHello3.java
@@ -1,8 +1,9 @@
 /**
  * 問16-3　<br>
- * 問16-2を用いて「おはよう！」、「こんにちわ！」、「こんばんわ！」<br>
+ * 問16-2を用いて「おはよう！」、「こんにちは！」、「こんばんは！」<br>
  * をそれぞれ表示するスレッド3つを動かすクラスprintHello3を宣言してください<br>
  */
+// 「修正」 コメントの「こんにちわ！」⇒「こんにちは！」、「こんばんわ！」⇒「こんばんわ！」に変更
 public class PrintHello3 {
     public static void main(String[] args) {
         LabelPrinter mh = new LabelPrinter("おはよう！");
