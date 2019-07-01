5abfb78ed6f611637d06e37cde0274f15eca3d9f
yasutaka.makino <yasutaka.makino@esm.co.jp>
2019-07-01 17:28:06 +0900

    修正箇所を記載しました。

--- 18syou/src/FindFile1.java
+++ 18syou/src/FindFile1.java
@@ -1,30 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.FileSystemNotFoundException;
 
 /**
  * 問18-2 ファイル中に特定の文字列を含んでいるかどうかを調べるプログラム
  */
 public class FindFile1 {
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("使用方:Java Find1 検索文字列 < 検索対象ファイル");
-            System.out.println("例:java Find1 System < Find1.java");
+        if (args.length != 2) {  //「修正」1⇒２に変更
+            System.out.println("使用法:Java FindFile1 検索文字列 < 検索対象ファイル");  //「修正」FindFile1に変更
+            System.out.println("例:java FindFind1 System < FindFile1.java"); //「修正」FindFile1に変更
             System.exit(0);
         }
         String keyword = args[0];
         System.out.println("検索文字列は「" + keyword + "」です。");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //「修正」tryの中に移動
             String r_line;
             int i = 1;
             while ((r_line = reader.readLine()) != null) {
                 if (r_line.contains(keyword)) {
                     System.out.println(i + ":" + r_line);
-                    //読み込みファイルを閉じる
                 }
                 i++;
             }
+        } catch (FileSystemNotFoundException e) { //「修正」  ファイルが開けなかった場合の例外処理を追加
+            System.out.println("ファイルが見つかりません" + e);
         } catch (IOException e) {
             System.out.println(e);
         }
